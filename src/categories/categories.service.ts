dc4c5f10387b1158f34e740892c509de6e407664
taewon <ktwjj2848@naver.com>
2024-01-11 17:09:30 +0900

    카테고리 로직오류 수정

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -26,6 +26,17 @@ export class CategoriesService {
       throw new CategoryAlreadyExistsException(exisingCategory.c_name);
     }
 
+    // c_id2가 null이 아닌 경우, c_id2가 가리키는 카테고리가 1차 카테고리인지 확인
+    if (createCategoryDto.c_id2) {
+      const category = await this.categoriesRepository.findOneBy({
+        c_id: createCategoryDto.c_id2,
+        c_id2: null,
+      });
+      if (!category) {
+        throw new CategoryNotFoundException(createCategoryDto.c_id2);
+      }
+    }
+
     const category = this.categoriesRepository.create(createCategoryDto);
     return this.categoriesRepository.save(category);
   }
@@ -43,20 +54,36 @@ export class CategoriesService {
   }
 
   async remove(id: number): Promise<string> {
+    // 카테고리가 존재하는지 확인
+    const category = await this.categoriesRepository.findOneBy({ c_id: id });
+    if (!category) {
+      throw new CategoryNotFoundException(id);
+    }
+    // 1차 카테고리 일 경우, 2차 카테고리로 참조하는 카테고리가 있는지 확인
+    if (category.c_id2 === null) {
+      const relatedCategories = await this.categoriesRepository.find({
+        where: { c_id2: id },
+      });
+      // 2차 카테고리로 참조하는 카테고리가 있으면 삭제 불가
+      if (relatedCategories.length > 0) {
+        throw new CategoryDeletionException(id, '카테고리');
+      }
+    }
+
     // 1차 카테고리로 참조하는 제품이 있는지 확인
     const relatedProductsPrimary = await this.productsRepository.find({
       where: { c_id: id },
     });
     // 1차 카테고리로 참조하는 제품이 있으면 삭제 불가
     if (relatedProductsPrimary.length > 0) {
-      throw new CategoryDeletionException(id);
+      throw new CategoryDeletionException(id, '제품');
     } else {
       // 2차 카테고리로 참조하는 제품이 있는지 확인
       const relatedProductsSecondary = await this.productsRepository.find({
         where: { c_id2: id },
       });
       if (relatedProductsSecondary.length > 0) {
-        throw new CategoryDeletionException(id);
+        throw new CategoryDeletionException(id, '제품');
       }
     }
 
